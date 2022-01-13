87c4b2d1514c400f56fb2e1e661990f28ef6ea3a
caseysparkz <himself@caseyspar.kz>
2022-01-13 15:53:16 -0800

    Added secrets, wine config.

--- vars.sh
+++ vars.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Personal defaults.
+# Application defaults.
 export EDITOR=vim
 export TERM=xterm-256color
+
+# Personal defaults.
 export ALIASES="${HOME}/.env/config/aliases"
+export ENV="${HOME}/.env"
 export FUNCTIONS="${HOME}/.env/functions"
-export SCRIPTS="${HOME}/.env/scripts"
 export GSH="${HOME}/.env/gsh"
 export SCREENSHOTS="${HOME}/Pictures/screenshots"
+export SCRIPTS="${HOME}/.env/scripts"
 export SECRETS="${HOME}/.env/secrets.asc"
 export VARS="${HOME}/.env/vars.sh"
-export ENV="${HOME}/.env"
+export WINEPREFIX="${HOME}/.config/wine"
 
 ## Docker variables.
 #export DOCKER_BUILDKIT=1

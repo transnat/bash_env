9e86bf167167913db913c78c9098baf3f6478b52
caseysparkz <himself@caseyspar.kz>
2022-06-30 10:01:52 -0700

    Updated branch script.

--- reconcile_branches.sh
+++ reconcile_branches.sh
@@ -5,6 +5,11 @@
 # Date:         June 14, 2022
 # Description:  Puts all git branches and remotes at current hostname HEAD.
 
+git switch ${HOSTNAME} &&
+    git add . &&
+    git commit &&
+    git push
+
 for BRANCH in $(git branch); do
     git switch ${BRANCH} &&
         git merge ${HOSTNAME} &&

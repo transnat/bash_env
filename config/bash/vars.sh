ddab6f80225e9b0f84c01dad85cb781c58d2b718
caseysparkz <himself@caseyspar.kz>
2023-04-21 13:32:31 -0700

    Changed secrets config.

--- config/bash/vars.sh
+++ config/bash/vars.sh
@@ -6,6 +6,7 @@ export TERM="xterm-256color"
 
 # Misc.
 export ALIASES="${HOME}/.env/config/bash/aliases"
+export BROWSER="$(which chrome)"
 export CONFIG="${HOME}/.env/config"
 export ENV="${HOME}/.env"
 export FUNCTIONS="${HOME}/.env/config/bash/functions"
@@ -14,7 +15,7 @@ export GSH="${HOME}/.env/gsh"
 export PGP_KEY_ID="0x3260255327CBCDA1"
 export SCREENSHOTS="${HOME}/Pictures/screenshots"
 export SCRIPTS="${HOME}/.env/scripts"
-export SECRETS="${HOME}/.env/config/secrets"
+export SECRETS="${HOME}/.env/config/secrets.asc"
 export VARS="${HOME}/.env/config/bash/vars.sh"
 
 ## Docker
